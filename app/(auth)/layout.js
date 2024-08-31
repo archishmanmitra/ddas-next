35d9e8825a895b366e6cdcf854c8cff24288995b
Atmajo <39727009+Atmajo@users.noreply.github.com>
2024-08-31 18:07:10 +0000

    chore: added authentication @clerk/nextjs

--- app/(auth)/layout.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function AuthLayout({children}) {
-  return (
-    <div className="w-full h-[90vh] flex items-center justify-center">
-      {children}
-    </div>
-  )
-}
--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
-
-import React from 'react'
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <div className='mt-6'>
-        <h1>Data Duplication Alert System YAYY</h1>
-      </div>
-    </main>
-  )
+    <div>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <Link href={"/sign-in"}>Sign In</Link>
+      </SignedOut>
+      <h1>Hello</h1>
+    </div>
+  );
 }
